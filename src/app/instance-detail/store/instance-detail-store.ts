e4d7f3e383ff7dd79ca882378b80a30e1caea225
Maurizio Cescon <maurizio.cescon@gmail.com>
2025-04-09 23:21:02 +0200

    chore: rxResource for loading blocks

--- src/app/instance-detail/store/instance-detail-store.ts
+++ src/app/instance-detail/store/instance-detail-store.ts
@@ -1,4 +1,5 @@
-import { computed, DestroyRef, inject, Injectable } from '@angular/core';
+import { computed, DestroyRef, inject, Injectable, linkedSignal, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 import { pipe } from 'rxjs';
 import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
@@ -6,18 +7,9 @@ import { patchState, signalState } from '@ngrx/signals';
 import { rxMethod } from '@ngrx/signals/rxjs-interop';
 import { tapResponse } from '@ngrx/operators';
 
-import { Block } from '../../shared/block';
-
 import { InstanceDetailDataClient } from './instance-detail-data-client';
 
 type State = {
-  loadParams: { instanceId: string | undefined };
-  loadedBlocks: Block<unknown>[];
-  loadOngoing: boolean;
-  loadError: string | undefined;
-
-  blocks: Block<unknown>[];
-
   syncParams: { instanceId: string | undefined };
   syncOngoing: boolean;
   syncError: string | undefined;
@@ -31,57 +23,44 @@ export class InstanceDetailStore {
   private readonly instanceDetail = inject(InstanceDetailDataClient);
 
   private readonly state = signalState<State>({
-    loadParams: { instanceId: undefined },
-    loadedBlocks: [],
-    loadOngoing: false,
-    loadError: undefined,
-
-    blocks: [],
-
     syncParams: { instanceId: undefined },
     syncOngoing: false,
     syncError: undefined,
   });
 
-  private readonly blocksValidity = computed(() => this.state.blocks()?.every(block => block.valid) ?? false);
+  private readonly params = signal<{ instanceId: string } | undefined>(undefined);
+  private readonly blocksResource = rxResource({
+    request: this.params,
+    loader: ({ request }) => this.instanceDetail.getBlocks(request!.instanceId),
+    defaultValue: [],
+  });
 
-  readonly editedBlocks = computed(() => this.state.blocks());
-  readonly isLoadingBlocks = computed(() => this.state.loadOngoing());
-  readonly loadingError = computed(() => this.state.loadError());
+  readonly editedBlocks = linkedSignal(() => this.blocksResource.value());
+  private readonly blocksValidity = computed(() => this.editedBlocks()?.every(block => block.valid) ?? false);
+
+  readonly isLoadingBlocks = computed(() => this.blocksResource.isLoading());
+  readonly loadingError = computed(() => this.blocksResource.error());
 
   readonly isSyncingBlocks = computed(() => this.state.syncOngoing());
   readonly syncingError = computed(() => this.state.syncError());
 
   readonly isSyncRequired = computed(() => this.state.syncParams().instanceId !== undefined);
   readonly isNextStepEnable = computed(() => this.blocksValidity() &&
-    !this.state.loadOngoing() && this.loadingError() === undefined &&
+    !this.blocksResource.isLoading() && this.loadingError() === undefined &&
     !this.state.syncOngoing() && this.state.syncError() === undefined);
-
-  private readonly loadBlocksSub = rxMethod<{ instanceId: string | undefined }>(
-    pipe(
-      filter(({ instanceId }) => instanceId !== undefined),
-      tap(() => patchState(this.state, { loadOngoing: true, loadError: undefined })),
-      switchMap(({ instanceId }) => this.instanceDetail.getBlocks(instanceId as string)
-        .pipe(
-          tapResponse({
-            next: data => patchState(this.state, { loadedBlocks: data, blocks: structuredClone(data) }),
-            error: (err: string) => patchState(this.state, { loadError: err }),
-            finalize: () => patchState(this.state, { loadOngoing: false }),
-          }),
-        ),
-      ),
-    ),
-  );
 
   private readonly syncBlocksSub = rxMethod<{ instanceId: string | undefined }>(
     pipe(
       filter(({ instanceId }) => instanceId !== undefined),
       tap(() => patchState(this.state, { syncOngoing: true, syncError: undefined })),
       debounceTime(3000),
-      switchMap(({ instanceId }) => this.instanceDetail.syncBlocks(instanceId as string, this.state.blocks())
+      switchMap(({ instanceId }) => this.instanceDetail.syncBlocks(instanceId as string, this.editedBlocks())
         .pipe(
           tapResponse({
-            next: data => patchState(this.state, { blocks: data, syncParams: { instanceId: undefined } }),
+            next: data => {
+              patchState(this.state, { syncParams: { instanceId: undefined } });
+              this.editedBlocks.set(data);
+            },
             error: (err: string) => patchState(this.state, { syncError: err }),
             finalize: () => patchState(this.state, { syncOngoing: false }),
           }),
@@ -91,28 +70,24 @@ export class InstanceDetailStore {
   );
 
   private readonly unregisterDestroy = this.destroyRef.onDestroy(() => {
-    this.loadBlocksSub?.destroy();
     this.syncBlocksSub?.destroy();
   });
 
   constructor() {
-    this.loadBlocksSub(this.state.loadParams);
     this.syncBlocksSub(this.state.syncParams);
   }
 
-  loadBlocks(data: { instanceId: string }) {
-    patchState(this.state, () => ({ loadParams: { instanceId: data.instanceId } }));
+  loadBlocks(params: { instanceId: string }) {
+    this.params.set(params);
   }
 
   updateBlock(data: { instanceId: string, blockId: string, value: unknown }) {
-    patchState(this.state, state => {
-      const blocks = [...state.blocks];
-      const blockIndex = blocks.findIndex(b => b.id === data.blockId);
-      if (blockIndex >= 0) {
-        blocks.splice(blockIndex, 1, { ...blocks[blockIndex], value: data.value });
-      }
-      return { blocks };
-    });
+    const blocks = [...this.editedBlocks()];
+    const blockIndex = blocks.findIndex(b => b.id === data.blockId);
+    if (blockIndex >= 0) {
+      blocks.splice(blockIndex, 1, { ...blocks[blockIndex], value: data.value });
+    }
+    this.editedBlocks.set(blocks);
     this.syncBlocks(data);
   }
 
